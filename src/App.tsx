83b0754087bb4f8b121c70b6d727301ddf69931b
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-09-11 17:49:52 -0500

    Use TaskForm and TaskList

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
+import type { TaskProps } from './types/Task'
 import { useState } from 'react'
+import { TodoForm } from './components/TaskForm'
+import { TaskList } from './components/TaskList'
 
 function App() {
-  
+  const [tasks, setTasks] = useState<TaskProps[]>([])
+
+  function addTask(task: TaskProps) {
+    setTasks([...tasks, task])
+  }
+
+  function updateTask(task: TaskProps) {
+    setTasks(tasks.map(t => t.id === task.id ? task : t))
+  }
+
+  function deleteTask(id: number) {
+    setTasks(tasks.filter(t => t.id !== id))
+  }
+
   return (
-    <>Hello</>
+    <div className="container">
+      <header>
+        <h1>My Todo List</h1>
+      </header>
+      <TodoForm addTask={addTask} />
+      {
+        tasks.length > 0 && (
+          <TaskList
+            tasks={tasks}
+            onUpdate={updateTask}
+            onDelete={deleteTask}
+          />
+        )
+      }
+    </div>
   )
 }
 

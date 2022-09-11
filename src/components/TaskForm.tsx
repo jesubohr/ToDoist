8d7bfd8fddee4ece702c3a0b161b3b29ec112abb
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-09-11 17:48:17 -0500

    Create TaskForm component

---
+++ src/components/TaskForm.tsx
@@ -0,0 +1,44 @@
+import type { TaskProps } from '../types/Task'
+import { useState } from 'react'
+import { PlusIcon } from '@heroicons/react/24/outline'
+
+export function TodoForm ({ addTask }: { addTask: (task: TaskProps) => void }) {
+  const [task, setTask] = useState("")
+
+  function submitHandler(event: React.FormEvent) {
+    event.preventDefault()
+    addTask({
+      id: Date.now(),
+      name: task,
+      checked: false
+    })
+    setTask("")
+  }
+
+  return (
+    <form className="todo" onSubmit={submitHandler}>
+      <div className="wrapper">
+        <input
+          type="text"
+          name="task"
+          id="task"
+          className="input"
+          value={task}
+          onInput={(ev: React.FormEvent) => setTask((ev.target as HTMLInputElement).value)}
+          required
+          autoFocus
+          maxLength={60}
+          placeholder="Enter a Task"
+        />
+        <label htmlFor="task" className="label">Enter a Task</label>
+      </div>
+      <button
+        type="submit"
+        className="btn"
+        aria-label="Add Task"
+      >
+        <PlusIcon />
+      </button>
+    </form>
+  )
+}

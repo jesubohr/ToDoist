59371dd054ca0971507286539ca7f62357e8be88
Jesús Borrero <jesufer2018@hotmail.com>
2023-03-11 12:30:03 -0500

    Add SEO and PWA tags and assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react-swc"
-import { VitePWA } from "vite-plugin-pwa"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,35 +9,32 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: [
-        "favicon.svg",
-        "favicon.ico",
-        "logo.png",
-        "waves.png",
-        "accept-terms.svg",
-        "robots.txt",
-        "apple-touch-icon.png"
+        'robots.txt',
+        'favicon.png',
+        'favicon.ico',
+        'apple-touch-icon.png',
       ],
       manifest: {
-        name: "ToDoist",
-        short_name: "ToDoist",
-        description: "Simple and elegant ToDo web app to manage your tasks.",
-        theme_color: "#8039c6",
+        name: 'ToDoist',
+        short_name: 'ToDoist',
+        description: 'Simple and elegant ToDo web app to manage your tasks.',
+        theme_color: '#7E00FF',
         icons: [
           {
-            src: "android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
           },
           {
-            src: "android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png"
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
           },
           {
-            src: "android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable"
+            src: 'favicon.png',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'maskable'
           }
         ]
       }
